725ef07cad1dfdc7bf05fc7ba2ef05a448c3bb1d
Carlos Ivan Chavez Fuentes <cachavezf@gmail.com>
2026-05-26 18:39:44 -0700

    Agregué un ciclo infinito al TCP server

--- U1A1: Socket TPC/tcp-socket/tcp-server.py
+++ U1A1: Socket TPC/tcp-socket/tcp-server.py
@@ -10,12 +10,12 @@ sock.listen(1)  # Máximo 1 conexión en cola
 
 print(f'[TCP] Servidor escuchando en {HOST}:{PORT}')
 
-conn, direccion = sock.accept()  # Bloquea hasta que un cliente se conecta
-print(f'[TCP] Conexión establecida con {direccion}')
+while True:
+    conn, direccion = sock.accept()  # Bloquea hasta que un cliente se conecta
+    print(f'[TCP] Conexión establecida con {direccion}')
 
-datos = conn.recv(1024)
-print(f'[TCP] Mensaje recibido: {datos.decode()}')
-conn.sendall(b'Mensaje recibido (TCP)')
+    datos = conn.recv(1024)
+    print(f'[TCP] Mensaje recibido: {datos.decode()}')
+    conn.sendall(b'Mensaje recibido (TCP)')
 
-conn.close()
-sock.close()
+    conn.close()
